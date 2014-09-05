d8c02cff96caf977ea7a5a25b618026448d3ed09
Rodion Gorkovenko <rodion@rg-home.(none)>
2014-09-05 09:32:11 +0400

    Change events processed for registers

--- src/main.js
+++ src/main.js
@@ -6,15 +6,23 @@ function AvrAsmDbg() {
 
 AvrAsmDbg.prototype.init = function() {
     this.opcodes = $('#opcodes');
+    for (var i = 0; i < 16; i++) {
+        this.addCodeLine(i);
+    }
     this.regs = $('#regs');
     for (var i = 0; i < 32; i++) {
-        $('<tr><td>' + i + '</td><td class="editable"></td><td class="editable"></td></tr>').appendTo(this.regs);
+        this.addRegsLine(i);
     }
     $('#opcodes').editableTableWidget();
     $('#regs').editableTableWidget();
+    $('#regs td').change(this.regsEdit);
 }
 
-AvrAsmDbg.prototype.addCodeLine = function() {
+AvrAsmDbg.prototype.addRegsLine = function(i) {
+    $('<tr><td>' + i + '</td><td class="editable"></td><td class="editable"></td></tr>').appendTo(this.regs);
+}
+
+AvrAsmDbg.prototype.addCodeLine = function(i) {
     $('<tr><td class="editable">&nbsp;</td><td class="editable"></td><td></td><td></td></tr>').appendTo(this.opcodes);
 }
 
@@ -39,13 +47,6 @@ AvrAsmDbg.prototype.populateCode = function() {
     var lines = this.avrasm.lines;
     var rows = this.opcodes.find('tr');
     var n = lines.length;
-    if (rows.size() - 1 < n) {
-        var toAdd = n - rows.size() - 1;
-        for (var j = 0; j < toAdd; j++) {
-            this.addCodeLine();
-        }
-        rows = this.opcodes.find('tr');
-    }
     for (var i = 0; i < n; i++) {
         var row = $(rows.get(i + 1)).find('td');
         var label = labels[i];
@@ -59,7 +60,7 @@ AvrAsmDbg.prototype.populateCode = function() {
     }
 }
 
-AvrAsmDbg.prototype.toHex  = function(v, d) {
+AvrAsmDbg.prototype.toHex = function(v, d) {
     var s = '00000000' + v.toString(16);
     return s.substr(s.length - d).toUpperCase();
 }
@@ -68,3 +69,25 @@ $(function() {
     avrAsmDbg = new AvrAsmDbg();
 });
 
+AvrAsmDbg.prototype.regsEdit = function() {
+    var $this = $(this);
+    var $tr = $this.parent();
+    var row = $tr.parent().children().index($tr) - 1;
+    var col = $tr.children().index($this);
+    var base, $hex, $dec;
+    if (col == 1) {
+        base = 16;
+        $hex = $this;
+        $dec = $this.next();
+    } else {
+        base = 10;
+        $hex = $this.prev();
+        $dec = $this;
+    }
+    var val = parseInt($this.text(), base);
+    if (isNaN(val) || val < 0 || val > 255) {
+        val = 0;
+    }
+    $dec.text(val.toString());
+    $hex.text(('00' + val.toString(16) + 'h').substr(-3));
+}
